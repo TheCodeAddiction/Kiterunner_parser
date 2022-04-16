402a369671e9162e9929cfff2dcfbf56c6b4e53d
jonalOW <jonalgaming@gmail.com>
2022-04-16 12:52:02 -0400

    Small tweak

--- inputHelper.py
+++ inputHelper.py
@@ -7,14 +7,30 @@ def getFilePath(user_arguments):
     return 0
 
 
+def getHelp(user_arugments):
+    helpFlag = "-h"
+    helpFlagv2 = "--help"
+    for arg in user_arugments:
+        if arg == helpFlag or arg == helpFlagv2:
+            return True
+    return False
+
+
+def printHelp():
+    print('-f <path_output_file>\tspesifiy the file you want filtered\n')
+    print('-hm method1,method2,method3...\tfilters on HTTP methods (GET, POST etc.)\n')
+    print('-c code1,code2,code3...\tfilters on HTTP Reponse code (200,404,301 etc.)\n')
+    print('-h or --help prints this message\n')
+
+
 def getHttpMethods(user_arguments):
     amountOfArgs = len(user_arguments)
-    httpFlag = "-h"
+    httpFlag = "-hm"
     for i in range(amountOfArgs):
         if user_arguments[i] == httpFlag:
             arr = user_arguments[i + 1].split(",")
             return arr
-    return 0
+    return False
 
 
 def getResponseCodes(user_arguments):
@@ -24,4 +40,4 @@ def getResponseCodes(user_arguments):
         if user_arguments[i] == responseCodeFlag:
             arr = user_arguments[i + 1].split(",")
             return arr
-    return 0
+    return False
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import re
 import sys
 
-from inputHelper import getFilePath, getHttpMethods, getResponseCodes
+from inputHelper import getFilePath, getHttpMethods, getResponseCodes, getHelp, printHelp
 from regexHelper import buildResponseCodeRegex, buildHttpMethodRegex
 
+if getHelp(sys.argv):
+    printHelp()
+    quit(1)
 pathToFile = getFilePath(sys.argv)
 httpMethods = getHttpMethods(sys.argv)
 responseCodes = getResponseCodes(sys.argv)
+httpMethodReg = ""
+responseCodeReg = ""
 
-responseCodeReg = buildResponseCodeRegex(responseCodes)
-httpMethodReg = buildHttpMethodRegex(httpMethods)
-file = open(pathToFile, "r")
-for line in file:
-    if re.findall(httpMethodReg, line):
-        print(line)
+if not pathToFile:
+    print("no file path, use -f to specify a path file")
+    quit(1)
+if httpMethods:
+    httpMethodReg = buildHttpMethodRegex(httpMethods)
+    file = open(pathToFile, "r")
+    for line in file:
+        if re.findall(httpMethodReg, line):
+            print(line)
+    file.close()
+
+if responseCodes:
+    responseCodeReg = buildResponseCodeRegex(responseCodes)
+    file = open(pathToFile, "r")
+    for line in file:
+        if re.findall(responseCodeReg, line):
+            print(line)
+    file.close()
+
